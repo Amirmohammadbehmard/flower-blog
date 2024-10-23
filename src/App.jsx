5b94be1dc29db74632089e3f0b631b30810b6213
amirmb <batbeginss@gmail.com>
2024-10-23 19:17:38 +0330

    final upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import { useRoutes } from "react-router-dom";
 import "./App.css";
 
 import { routesItem } from "./Routes";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    setInterval(() => {
-      alert("im in the => ", window.location.pathname);
-    }, 15000);
-  }, []);
   let routes = useRoutes(routesItem);
   return routes;
 }
